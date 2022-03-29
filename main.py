f03978502f602f5d3f863105a372bdd05d2b4968
niko89081 <90409933+niko89081@users.noreply.github.com>
2022-03-29 11:20:27 -0400

    main.py
    this is the main file to detect curvywurvy lineywineys

---
+++ main.py
@@ -0,0 +1,34 @@
+import cv2 as cv
+import numpy as np
+
+"""
+   import numpy as np
+   import cv2
+
+   # Read the main image
+
+   inputImage = cv2.imread("input.png")
+
+   # Convert it to grayscale
+
+   inputImageGray = cv2.cvtColor(inputImage, cv2.COLOR_BGR2GRAY)
+
+   # Line Detection
+
+   edges = cv2.Canny(inputImageGray,100,200,apertureSize = 3)
+
+   minLineLength = 500 
+   maxLineGap = 5 
+
+   lines = cv2.HoughLinesP(edges,1,np.pi/180,90,minLineLength,maxLineGap)
+
+   for x in range(0, len(lines)):
+       for x1,y1,x2,y2 in lines[x]:
+           cv2.line(inputImage,(x1,y1),(x2,y2),(0,128,0),2)
+
+   cv2.putText(inputImage,"Tracks Detected", (500,250), font, 0.5, 255)
+
+   # Show result
+
+   cv2.imshow("Trolley_Problem_Result", inputImage)
+"""
